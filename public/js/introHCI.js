32adcd293f2feb670553e207491598eb14cadee0
wscottmiller <wmiller@ucsd.edu>
2016-02-11 20:42:25 +0000

    finished lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -23,10 +23,35 @@ function addProjectDetails(e) {
 
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
+	
+	console.log("projectID " + projectID);
+
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result){
+
+	console.log("project" + result.id);
+
+	var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
+
+    var projectContainer = '<p>' +
+    result['summary'] +
+	'</p>';
+
+	$('#project' + result.id + " .details").html(projectHTML);
+
+	$('#project' + result.id + " .details").append(projectContainer);
+
 }
 
 /*
@@ -34,5 +59,27 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+
 	console.log("User clicked on color button");
+
+	$.get("/palette/" , colors);
+
+}
+
+
+function colors(result){
+
+	//console.log("Debug message");
+	var colors = result.colors.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
+
+
+
+
 }
